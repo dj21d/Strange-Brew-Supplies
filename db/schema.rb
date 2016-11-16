a6a3455c95a6b25dc18a916342384c8b2970e2dd
dj21d <derekrichard@shaw.ca>
2016-11-16 04:36:09 -0600

    Added static page model type to page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116090046) do
+ActiveRecord::Schema.define(version: 20161116103112) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -145,6 +145,13 @@ ActiveRecord::Schema.define(version: 20161116090046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+  end
+
+  create_table "static_pages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
