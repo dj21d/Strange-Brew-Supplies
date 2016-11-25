d87cda4de432797ddd68cf5b6a87de059f9e24d2
dj21d <derekrichard@shaw.ca>
2016-11-25 03:07:08 -0600

    Added password columns to customer model

--- db/migrate/20161125090515_add_columns_to_customers.rb
+++ db/migrate/20161125090515_add_columns_to_customers.rb
@@ -1,5 +1,6 @@
 class AddColumnsToCustomers < ActiveRecord::Migration[5.0]
   def change
     add_column :customers, :encrypted_password, :string
+    add_column :customers, :salt, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125080528) do
+ActiveRecord::Schema.define(version: 20161125090515) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -61,8 +61,10 @@ ActiveRecord::Schema.define(version: 20161125080528) do
     t.string   "full_name"
     t.string   "email"
     t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   create_table "equipment", force: :cascade do |t|
