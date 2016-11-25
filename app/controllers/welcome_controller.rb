37d4797c720274b467b53a17ab27538b773dbc99
dj21d <derekrichard@shaw.ca>
2016-11-25 05:18:55 -0600

    Fixed new items on index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @on_sale_ingredients = Ingredient.all
-    @new_items = Ingredient.find_by!(:updated_at > 5.days.ago)
+    @ingredients = Ingredient.all
   end
 end
