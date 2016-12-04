c573528ca5deb8f31e1eb97b2c72d405d98077ff
dj21d <derekrichard@shaw.ca>
2016-12-04 13:25:18 -0600

    Added model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
   has_many :cust_addresses
-  validates :full_name,presence: true
-
+  validates :full_name,:email,:password_digest,presence: true
+  validates :email, uniqueness: true
   has_secure_password
 end
